435d1d039c6badba49350f7abb48755c59c018a5
LeviPeralta <leviytp@gmail.com>
2025-12-08 22:37:18 -0600

    argegue el syngle list

---
+++ src/main/java/com/fastfood/fastfoodservice/datastructures/synglelinkedlist/ListNode.java
@@ -0,0 +1,21 @@
+package com.fastfood.fastfoodservice.datastructures.synglelinkedlist;
+
+import com.fastfood.fastfoodservice.model.Pedido;
+
+public class ListNode {
+    Pedido data;
+    ListNode next;
+
+    public ListNode(Pedido data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Pedido getData() {
+        return data;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+}
