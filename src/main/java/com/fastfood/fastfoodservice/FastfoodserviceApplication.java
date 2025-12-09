6dd430d5d5c404b7b0e56d2ade3a157e48ccffa8
LeviPeralta <leviytp@gmail.com>
2025-12-08 21:38:55 -0600

    first commit

---
+++ src/main/java/com/fastfood/fastfoodservice/FastfoodserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fastfood.fastfoodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastfood/fastfoodservice/FastfoodserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastfood.fastfoodservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfoodserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
